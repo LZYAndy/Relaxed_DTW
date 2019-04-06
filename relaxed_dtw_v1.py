e9989fe9504384553389ade35ad15888207ecfcb
varsha33 <varshasuresh2706@gmail.com>
2019-04-06 17:43:57 +0800

    Update relaxed_dtw_v1.py

--- relaxed_dtw_v1.py
+++ relaxed_dtw_v1.py
@@ -5,15 +5,9 @@ def	relaxed_dtw(x,y,distance = lambda x,y : ((x-y)**2), r=0):
 	# dist : metric used to calculate the distance, default = (x-y)**2
 	# r : relaxation factor, default is 0 which is classic DTW
 
-	import pandas as pd
 	import numpy as np
-	import matplotlib.pyplot as plt
-	import seaborn as sns
 
-	DTW = np.zeros((len(y), len(x)))
-	for i in range(0,len(y)):
-	    for j in range(0,len(x)):
-	        DTW[i,j] = np.inf
+	DTW = np.full((len(y), len(x)),np.inf)
 
 	if (r==0):  ## classic DTW 
 	    DTW[0:,0]= 0
@@ -24,8 +18,7 @@ def	relaxed_dtw(x,y,distance = lambda x,y : ((x-y)**2), r=0):
 
 	for i in range(1, len(y)):
 	    for j in range(1, len(x)):
-	        cost = distance(x[j],y[i])
-	        DTW[i,j] = cost + min(DTW[i-1, j-1],DTW[i-1, j], DTW[i, j-1])
+	        DTW[i,j] = distance(x[j],y[i]) + min(DTW[i-1, j-1],DTW[i-1, j], DTW[i, j-1])
 
 	if (r!=0):
 	    min_x = min(DTW[len(y)-1,len(x)-r-1:len(x)-1])
